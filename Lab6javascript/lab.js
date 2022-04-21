77305d2266b955315165d90914374e7d7cf27432
tahirftw <65671200+tahirftw@users.noreply.github.com>
2022-04-21 19:00:44 +0500

    Create lab.js

---
+++ Lab6javascript/lab.js
@@ -0,0 +1,21 @@
+function math(){
+    a=Number(document.my_cal.first.value);
+    b=Number(document.my_cal.second.value);
+    c=a*b;
+    document.my_cal.mul.value=c;
+  
+    d=a+b;
+    document.my_cal.add.value=d;
+  
+
+
+  
+    e=a-b;
+    document.my_cal.sub.value=e;
+  
+
+
+
+    f=a/b;
+    document.my_cal.div.value=f;
+  }
