88e4ea53c1748a6ae01f413174a60e7301ced09d
tahirftw <65671200+tahirftw@users.noreply.github.com>
2022-04-21 23:42:30 +0500

    Update lab.js

--- Lab6javascript/lab.js
+++ Lab6javascript/lab.js
@@ -6,16 +6,34 @@ function math(){
   
     d=a+b;
     document.my_cal.add.value=d;
-  
-
-
-  
+ 
     e=a-b;
     document.my_cal.sub.value=e;
   
 
-
-
     f=a/b;
     document.my_cal.div.value=f;
   }
+  function popup(){
+    alert("JS alert box");
+
+
+  }
+  function Min_Max(){
+    var numOne, numTwo, numThree, numFour, numFive, sum;
+    one = parseInt(document.my_calc.firstnum.value);
+    two = parseInt(document.my_calc.secondnum.value);
+    three = parseInt(document.my_calc.thirdnum.value);
+    four = parseInt(document.my_calc.fourthnum.value);
+    five = parseInt(document.my_calc.fifthnum.value);
+    let a = Math.max(one, two, three, four, five);
+    let b = Math.min(one, two, three, four, five);
+    const random = [one, two, three, four, five];
+    var index = Math.floor(Math.random() *random.length);
+    c = random[index];
+    document.my_calc.a.value = a;
+    document.my_calc.b.value = b;
+    document.my_calc.c.value = c;
+  }
+  
+
